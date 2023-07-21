65ad05aa9a7efb0ebfedafdaa297cc6ed498c8df
hansejun <tpwnszja2@gmail.com>
2023-07-22 01:08:30 +0900

    Add: 검색 기능 추가

---
+++ hooks/useDebounce.ts
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+
+const useDebounce = <T>(value: T, delay?: number): T => {
+  const [debounceValue, setDebounceValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebounceValue(value);
+    }, delay || 500);
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+
+  return debounceValue;
+};
+
+export default useDebounce;
